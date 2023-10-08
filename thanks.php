0133ee5f74d16899a77c7f631bda1f12e4d4b4aa
StyMey <stephanie2510@gmail.com>
2023-10-08 23:58:15 +0200

    re-modif echo

--- thanks.php
+++ thanks.php
@@ -1,3 +1,3 @@
 <?php
 
-echo 'Merci ' . 'user_firstname' . ' ' . 'user_name' . ' ' . 'de nous avoir contacté à propos de ' . 'Languages' . '<br>' . 'Un de nos conseillers vous contactera soit à l\'adresse' . ' ' . 'user_email' . ' ' . 'ou par téléphone au ' . 'phone_number' . ' ' . 'dans les plus brefs délais pour traiter votre demande' . '<br>' . 'user_message';
+echo 'Merci ' . $_POST['user_firstname'] . ' ' . $_POST['user_name'] . ' ' . 'de nous avoir contacté à propos de ' . $_POST['Languages'] . '<br>' . 'Un de nos conseillers vous contactera soit à l\'adresse' . ' ' . $_POST['user_email'] . ' ' . 'ou par téléphone au ' . $_POST['phone_number'] . ' ' . 'dans les plus brefs délais pour traiter votre demande' . '<br>' . $_POST['user_message'];
